4866758382e0f298d5aa64e35fc4b14e67910e8c
Cameroncbrownjr <cameroncbrownjr@gmail.com>
2019-09-03 03:43:39 -0400

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgersController");
+// heroku
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+  console.log("listening " + PORT);
+});
