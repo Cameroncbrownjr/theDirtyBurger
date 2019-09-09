7df1f86b16cc6b5a06780743f89a4a8b97e3f37a
Cameroncbrownjr <cameroncbrownjr@gmail.com>
2019-09-09 00:45:09 -0400

    cleardb

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.CLEAR_DATABASE_URL) {
+  connection = mysql.createConnection(process.env.CLEAR_DATABASE_URL);
 }
 else {
   connection = mysql.createConnection({
